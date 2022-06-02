78edfdf7175446b505557525aaf69f365d231b9f
COLDF1RE <coldfire13@mail.ru>
2022-06-02 15:41:31 +0300

    Fix: исправлены иконки приоритета

--- src/components/UI/Rank.jsx
+++ src/components/UI/Rank.jsx
@@ -11,12 +11,12 @@ const Rank = ({rank}) => {
                 }
                 {rank === 'medium' &&
                 <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7 8L14 0L7 4L0 0L7 8Z" fill="#3CA961"/>
+                    <path d="M7 0L0 3L7 6L14 3L7 0Z" fill="#E9B500"/>
                 </svg>
                 }
                 {rank === 'high' &&
                 <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7 8L14 0L7 4L0 0L7 8Z" fill="#3CA961"/>
+                    <path d="M7 0L0 8L7 4L14 8L7 0Z" fill="#D14343"/>
                 </svg>
                 }
             </div>
